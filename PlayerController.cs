458146d9fb3b5af94983dd5d09fcde3c44af3f44
Andy Atari <andrewsalteri@gmail.com>
2021-05-22 17:41:30 -0400

    Added features that take in user input and move the vehicle forward, backward, and turns.

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.65f;
+    // Private variables 
+    private float speed = 15.65f;
+    private float turnSpeed = 25.02f;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Initialized input variables to get horizontal and vertical input 
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward based on vertical input 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotated the vehicle based on horizontal input 
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
+
+   
