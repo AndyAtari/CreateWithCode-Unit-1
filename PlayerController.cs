3b5052715e0c6e6b3cff708c2b6f77ea9e1cafe3
Andy Atari <andrewsalteri@gmail.com>
2021-05-26 20:12:44 -0400

    Added an if loop that switches camera perspective on KeyDown.

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private variables 
-    private float speed = 15.65f;
+    private float speed = 20.0f;
     private float turnSpeed = 25.02f;
     private float horizontalInput;
     private float forwardInput;
+
+    // Public variables
+    public Camera mainCamera;
+    public Camera firstPersonCamera;
+    public KeyCode switchKey; 
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +27,20 @@ public class PlayerController : MonoBehaviour
         // Initialized input variables to get horizontal and vertical input 
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+        
 
         // Move the vehicle forward based on vertical input 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
         // Rotated the vehicle based on horizontal input 
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        
+        // Switch camera perspective
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            firstPersonCamera.enabled = !firstPersonCamera.enabled;
+        }
     }
 }
 
